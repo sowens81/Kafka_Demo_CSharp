78a35fb8ab03132d5de8faba8f70766838cda57d
Steve Owens <steven_owens@epam.com>
2020-11-02 12:31:32 +0000

    Update build pipeline

---
+++ src/Subscriber/Settings/IDBSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Subscriber.Settings
+{
+    public interface IDBSettings
+    {
+        string CollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
